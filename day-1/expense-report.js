d41670493fa5effe27b28fc1d86d691b868c71b6
Alex Olshansky <alex@creagenics.com>
2020-12-01 18:56:21 +0100

    Day 1 Part 2

--- day-1/expense-report.js
+++ day-1/expense-report.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const entries = data.trim().split(/\r?\n/).map(i => +i);
 
-for (let i = 0; i < entries.length - 1; i++) {
-  for (let j = i + 1; j < entries.length; j++) {
-    if (entries[i] + entries[j] === 2020) {
-      console.log(entries[i] * entries[j]);
+for (let i = 0; i < entries.length - 2; i++) {
+  for (let j = i + 1; j < entries.length - 1; j++) {
+    for (let k = j + 1; k < entries.length; k++) {
+      if (entries[i] + entries[j] + entries[k] === 2020) {
+        console.log(entries[i] * entries[j] * entries[k]);
+      }
     }
   }
 }
