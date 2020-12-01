b3324e82c727914f97988046a7174acbdb32ccab
Alex Olshansky <alex@creagenics.com>
2020-12-01 23:11:16 +0100

    Day 1 Part 2 improved: O(n^3) -> O(n^2)

--- day-1/expense-report.js
+++ day-1/expense-report.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const entries = data.trim().split(/\r?\n/).map(i => +i);
+const set = new Set(entries);
 
-for (let i = 0; i < entries.length - 2; i++) {
-  for (let j = i + 1; j < entries.length - 1; j++) {
-    for (let k = j + 1; k < entries.length; k++) {
-      if (entries[i] + entries[j] + entries[k] === 2020) {
-        console.log(entries[i] * entries[j] * entries[k]);
-      }
+for (let i = 0; i < entries.length - 1; i++) {
+  for (let j = i + 1; j < entries.length; j++) {
+    const thirdOne = 2020 - entries[i] - entries[j];
+    if (set.has(thirdOne)) {
+      console.log(entries[i] * entries[j] * thirdOne);
+      return;
     }
   }
 }
