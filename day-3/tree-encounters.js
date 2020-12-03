01564d67992f44051c1034cd78989ea8365353ad
Alex Olshansky <alex@creagenics.com>
2020-12-03 18:58:22 +0100

    Day 3 Part 2

--- day-3/tree-encounters.js
+++ day-3/tree-encounters.js
@@ -2,15 +2,17 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const map = data.trim().split(/\r?\n/);
 
-const stepsRight = 3;
-const stepsDown = 1;
-
-let count = 0;
-for (let x = stepsRight, y = stepsDown; y < map.length; y += stepsDown, x += stepsRight) {
-  const xNorm = x % map[y].length;
-  if (map[y][xNorm] === '#') {
-    count++;
+const countSlope = (stepsRight, stepsDown) => {
+  let count = 0;
+  for (let x = stepsRight, y = stepsDown; y < map.length; y += stepsDown, x += stepsRight) {
+    const xNorm = x % map[y].length;
+    if (map[y][xNorm] === '#') {
+      count++;
+    }
   }
+  return count;
 }
 
-console.log(count);
+const multiplied = countSlope(1, 1) * countSlope(3, 1) * countSlope(5, 1) * countSlope(7, 1) * countSlope(1, 2);
+
+console.log(multiplied);
