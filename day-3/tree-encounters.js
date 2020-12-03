e82a5a0fb8f7e0dfa7aa4d8f5dd56d5a3dc59922
Alex Olshansky <alex@creagenics.com>
2020-12-03 18:56:01 +0100

    Day 3 Part 1: Toboggan Trajectory

---
+++ day-3/tree-encounters.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
+const map = data.trim().split(/\r?\n/);
+
+const stepsRight = 3;
+const stepsDown = 1;
+
+let count = 0;
+for (let x = stepsRight, y = stepsDown; y < map.length; y += stepsDown, x += stepsRight) {
+  const xNorm = x % map[y].length;
+  if (map[y][xNorm] === '#') {
+    count++;
+  }
+}
+
+console.log(count);
