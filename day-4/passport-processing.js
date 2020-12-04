3d434afadb3329aac3618ff590347d2b8c87f6c4
Alex Olshansky <alex@creagenics.com>
2020-12-04 16:46:12 +0100

    Day 4 Part 2

--- day-4/passport-processing.js
+++ day-4/passport-processing.js
@@ -2,11 +2,22 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const entries = data.trim().split(/\r?\n\r?\n/);
 
-const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+const rules = [
+  ['byr', /^(19[2-9][0-9]|200[0-2])$/],
+  ['iyr', /^20(1[0-9]|20)$/],
+  ['eyr', /^20(2[0-9]|30)$/],
+  ['hgt', /^(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)$/],
+  ['hcl', /^#[\da-f]{6}$/],
+  ['ecl', /^(amb|blu|brn|gry|grn|hzl|oth)$/],
+  ['pid', /^\d{9}$/],
+];
 
 const checkEntry = (entry) => {
-  const presentFields = entry.match(/\b(\w+):/g);
-  return requiredFields.every(f => presentFields.includes(f+':'));
+  const passport = [...entry.matchAll(/\b(\w+):([^\s]+)/g)] // -> array of arrays of strings
+    .map(m => ({[m[1]]: m[2]})) // -> array of objects with one field
+    .reduce((acc, obj) => ({...acc, ...obj}), {}); // -> object with all fields
+
+  return rules.every(([field, rule]) => passport[field]?.match(rule));
 };
 
 const count = entries.filter(checkEntry).length;
