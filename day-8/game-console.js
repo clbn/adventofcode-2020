067c0a9ba471bcb9f3be10d78f695b2e4dbc4857
Alex Olshansky <alex@creagenics.com>
2020-12-08 09:40:02 +0100

    Day 8 Part 2

--- day-8/game-console.js
+++ day-8/game-console.js
@@ -7,18 +7,38 @@ const instructions = entries.map(e => {
   return { operation, argument: +argument };
 })
 
-const visited = new Set();
-let accumulator = 0;
-let position = 0;
+const run = (instructions) => {
+  const visited = new Set();
+  let accumulator = 0;
+  let position = 0;
 
-while (!visited.has(position)) {
-  visited.add(position);
-  const { operation, argument } = instructions[position];
-  switch (operation) {
-    case 'acc': accumulator += argument; position++; break;
-    case 'jmp': position += argument; break;
-    case 'nop': position++; break;
+  while (!visited.has(position)) {
+    if (position === instructions.length) {
+      return { accumulator, corrupted: false };
+    }
+
+    visited.add(position);
+    const { operation, argument } = instructions[position];
+    switch (operation) {
+      case 'acc': accumulator += argument; position++; break;
+      case 'jmp': position += argument; break;
+      case 'nop': position++; break;
+    }
   }
+
+  return { accumulator, corrupted: true };
 }
 
-console.log(accumulator);
+for (let i = 0; i < instructions.length; i++) {
+  const { operation, argument } = instructions[i];
+  if (operation !== 'acc') {
+    const repaired = [...instructions];
+    repaired[i] = { argument, operation: operation === 'nop' ? 'jmp' : 'nop' };
+
+    const { accumulator, corrupted } = run(repaired);
+    if (!corrupted) {
+      console.log(accumulator);
+      return;
+    }
+  }
+}
