c4e9c157b0ed76b43f37deec09b5e650378589ea
Alex Olshansky <alex@creagenics.com>
2020-12-13 08:18:03 +0100

    Day 13 Part 2

--- day-13/bus-schedule.js
+++ day-13/bus-schedule.js
@@ -2,10 +2,28 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const rows = data.trim().split(/\r?\n/);
 
-const timestamp = rows[0];
-const buses = rows[1].split(',').map(i => +i).filter(Boolean);
+// --- Part One ---
 
+let timestamp = rows[0];
+let buses = rows[1].split(',').map(i => +i).filter(Boolean);
 const waitingTimes = buses.map(b => b - timestamp % b);
 const earliest = waitingTimes.indexOf(Math.min(...waitingTimes));
 
 console.log(buses[earliest] * waitingTimes[earliest]);
+
+// --- Part Two ---
+
+timestamp = 1;
+buses = rows[1].split(',').map(i => +i);
+let step = 1;
+
+for (let i=0; i < buses.length; i++) {
+  for (; buses[i]; timestamp += step) {
+    if ((timestamp + i) % buses[i] === 0) {
+      step *= buses[i];
+      break;
+    }
+  }
+}
+
+console.log(timestamp);
