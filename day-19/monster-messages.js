87d24a07e4a5518a6a9be7bbee81fd6f012e18ce
Alex Olshansky <alex@creagenics.com>
2020-12-19 22:50:57 +0100

    Day 19: Monster Messages (minor refactoring)

--- day-19/monster-messages.js
+++ day-19/monster-messages.js
@@ -5,33 +5,25 @@ const [rawRules, rawMessages] = data.trim().split(/\r?\n\r?\n/);
 const rules = [];
 rawRules.split(/\r?\n/).forEach(r => {
   const [, number, format] = r.match(/^(\d+): (.+)$/);
-  rules[+number] = format;
+  rules[number] = format.includes('|') ? `( ${format} )` : format;
 });
-rules[8] = '( 42 ) +';
-rules[11] = '( 42 ( 42 ( 42 ( 42 31 ) ? 31 ) ? 31 ) ? 31 )';
+rules[8] = '42 +';
+rules[11] = '42 ( 42 ( 42 ( 42 31 ) ? 31 ) ? 31 ) ? 31';
 
 const messages = rawMessages.split(/\r?\n/);
 
-const parse = (n) => {
-  n = +n;
-  let hasPipe = false;
-
-  const parsed = rules[n]
-    .split(' ')
-    .map(t => {
-      if (t[0] === '"') {
-        return t[1];
-      }
-      if (/\d/.test(t)) {
-        return parse(t);
-      }
-      hasPipe = true;
-      return t;
-    })
-    .join('');
-
-  return hasPipe ? `(${parsed})` : parsed;
-}
+const parse = (n) => rules[n]
+  .split(' ')
+  .map(t => {
+    if (t[0] === '"') {
+      return t[1];
+    }
+    if (/\d/.test(t)) {
+      return parse(t);
+    }
+    return t;
+  })
+  .join('');
 
 const theRule = parse(0);
 const theRegex = new RegExp('^' + theRule + '$');
